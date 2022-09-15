fcd570d92a3de7405a15bfcd3872a7d1d2626274
Yatharth <52836348+yatharth-16@users.noreply.github.com>
2022-09-15 18:05:53 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,22 @@
+
+
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomImageSource1 = "images/"+"dice"+randomNumber1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImageSource2 = "images/"+"dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// var randomImageSource2 = "images/" + "dice"+Math.floor(Math.random()*6)+1+".png";
+//
+// document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+if(randomNumber1>randomNumber2)
+document.querySelector("h1").innerHTML = "Player 1 Wins ! 🚩";
+else if(randomNumber1<randomNumber2)
+document.querySelector("h1").innerHTML = "Player 2 Wins ! 🎌";
+else
+document.querySelector("h1").innerHTML = "Draw ! 🏴‍☠️";
